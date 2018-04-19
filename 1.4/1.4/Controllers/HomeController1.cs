58bee2a97c6b9217d0652a205dcfc18722d1c134
herrnkusmin <36639928+herrnkusmin@users.noreply.github.com>
2018-04-19 02:53:07 +0200

    Add files via upload

---
+++ 1.4/1.4/Controllers/HomeController1.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ <- bisheriger Link
+
+        public IActionResult Index(string answer)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Login Token
+        public ActionResult ProcessForm(string login, string register)
+        {
+            if(!string.IsNullOrEmpty(login)) //TODO if Login is pressed
+            {
+
+            }
+            if (!string.IsNullOrEmpty(register))//TODO if Register is pressed
+            {
+
+            }
+            return View("Index");
+        }
+
+        
+    }
+}
